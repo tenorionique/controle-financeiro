150d12ac9cc77a5043f98a2af23ec92d568a51d4
tenorionique <monique_doretto@hotmail.com>
2021-12-01 14:43:53 -0300

    transaction name and amount

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-const transactionsUl = document.querySelector('#transactions')
+const transactionsUl = document.querySelector('#transactions');
+const incomeDisplay = document.querySelector('#money-plus');
+const expenseDisplay = document.querySelector('#money-minus');
+const balanceDisplay = document.querySelector('#balance');
+const form = document.querySelector('#form');
+const inputTransactionName = document.querySelector('#text');
+const inputTransactionAmount = document.querySelector('#amount');
+
 
 const dummyTransactions = [
   {id: 1, name: 'Bolo de brigadeiro', amount: -20},
@@ -22,11 +29,53 @@ const addTransactionIntoDOM = transaction => {
 }
 
 const updateBalanceValues = () => {
+  const transactionsAmounts = dummyTransactions
+    .map(transaction => transaction.amount)
+  const total = transactionsAmounts
+    .reduce((accumulator, transaction) => accumulator + transaction, 0)
+    .toFixed(2)
+  const income = transactionsAmounts
+    .filter(value => value > 0)
+    .reduce((accumulator, value) => accumulator + value, 0)
+    .toFixed(2)
+  const expense = Math.abs(
+    transactionsAmounts
+    .filter(value => value < 0)
+    .reduce((accumulator, value) => accumulator + value, 0))
+    .toFixed(2) 
   
-}
+  
+  balanceDisplay.textContent = `R$ ${total}`  
+  incomeDisplay.textContent = `R$ ${income}`  
+  expenseDisplay.textContent = `R$ ${expense}`  
 
+}
+ 
 const init = () => {
   dummyTransactions.forEach(addTransactionIntoDOM)
+  updateBalanceValues( )
 }
 
 init()
+
+const generateID = () => Math.round(Math.random() * 1000)
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  
+  const transactionName = inputTransactionName.value.trim();
+  const transactionAmount = inputTransactionAmount.value.trim()
+
+  if(transactionName === '' || transactionAmount === '' ) {
+    alert('Por favor, preencha tanto o nome quanto o valor da transação')
+    return
+  }
+
+  const transaction =  { 
+    id: generateID(), 
+    name: transactionName, 
+    amount: transactionAmount 
+  }
+ 
+  dummyTransactions.push(transaction)
+})
